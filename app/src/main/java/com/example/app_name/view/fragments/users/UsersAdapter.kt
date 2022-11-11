bb49a620bb3e10f2bf0ee2c4f4844dd3bd4e5702
ammar <ammarishfaq25@gmail.com>
2022-11-11 17:22:38 +0500

    - latest code commit

--- app/src/main/java/com/example/app_name/view/fragments/users/UsersAdapter.kt
+++ app/src/main/java/com/example/app_name/view/fragments/users/UsersAdapter.kt
@@ -1,6 +1,5 @@
 package com.example.app_name.view.fragments.users
 
-import android.graphics.drawable.Drawable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,11 +7,6 @@ import androidx.recyclerview.widget.AsyncListDiffer
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
-import com.bumptech.glide.load.DataSource
-import com.bumptech.glide.load.engine.DiskCacheStrategy
-import com.bumptech.glide.load.engine.GlideException
-import com.bumptech.glide.request.RequestListener
-import com.bumptech.glide.request.target.Target
 import com.example.app_name.R
 import com.example.app_name.models.usersModel.AllUserResponseItem
 import kotlinx.android.synthetic.main.item_users.view.*
